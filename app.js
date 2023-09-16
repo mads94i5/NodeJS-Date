83e91c3935abf4f41718bde389a6bfb94fcb00ad
mads94i5 <mads94i5@stud.kea.dk>
2023-09-16 12:37:55 +0200

    vercel sucks

--- app.js
+++ app.js
@@ -1,31 +1,139 @@
-const { calcDate, getAgeFromDate } = require("./utils/utils");
+// const { calcDate, getAgeFromDate } = require("./utils/utils");
 
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
+
+const API_URL = "https://node-js-date.vercel.app/api/";
+// const API_URL = "http://localhost:8080/api/";
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/home/index.html");
+  res.sendFile(__dirname + "/public/home/index.html");
 });
 
 app.get("/datecalc", (req, res) => {
-    res.sendFile(__dirname + "/public/datecalc/index.html");
+  res.sendFile(__dirname + "/public/datecalc/index.html");
+  initDateCalc();
 });
 
 app.post("/api/datecalc", (req, res) => {
-    isDateAcceptable = calcDate(getAgeFromDate(req.body.yourBirthDate), getAgeFromDate(req.body.dateBirthDate));
-    res.send({ data: isDateAcceptable });
+  isDateAcceptable = calcDate(
+    getAgeFromDate(req.body.yourBirthDate),
+    getAgeFromDate(req.body.dateBirthDate)
+  );
+  res.send({ data: isDateAcceptable });
 });
 
+let dateCalcInitialized = false;
+
+async function initDateCalc() {
+  const form = document.querySelector("#form");
+  const resultContainer = document.getElementById("date-result");
+  if (!dateCalcInitialized) {
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetchPostJsonFormData(
+          API_URL + "datecalc",
+          form
+        );
+        if (response) {
+          resultContainer.innerText = response.data;
+        } else {
+          resultContainer.innerText = response.message;
+        }
+      } catch (error) {
+        resultContainer.innerText = error.message;
+      }
+    });
+    dateCalcInitialized = true;
+  }
+}
+
+function calcDate(yourAge, dateAge) {
+  lowestRecommendedAge = yourAge / 2 + 7;
+  if (dateAge < 15) {
+    return "Your date is too young to be dating, you disgusting pig!";
+  } else if (yourAge < 15) {
+    return "You are too young to be dating.";
+  } else if (yourAge < dateAge) {
+    return "Your date is older than you.";
+  } else if (yourAge == dateAge) {
+    return "Your date is same age as you.";
+  } else if (lowestRecommendedAge > dateAge) {
+    return "Your date is outside the acceptable age range, you disgusting pig!";
+  } else {
+    return "Your date is within the acceptable age range.";
+  }
+}
+
+function getAgeFromDate(date) {
+  const currentDate = new Date();
+  const inputDate = new Date(date);
+
+  if (
+    inputDate.getDate() < currentDate.getDate() &&
+    inputDate.getMonth() < currentDate.getMonth()
+  ) {
+    return currentDate.getFullYear() - inputDate.getFullYear() - 1;
+  } else {
+    return currentDate.getFullYear() - inputDate.getFullYear();
+  }
+}
+
+export async function handleHttpErrors(res) {
+  if (!res.ok) {
+    const errorResponse = await res.json();
+    const error = new Error(errorResponse.message);
+    // @ts-ignore
+    error.fullResponse = errorResponse;
+    throw error;
+  }
+  return res.json();
+}
+
+export async function fetchGetJson(URL, token = null) {
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (token) {
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+  const data = await fetch(URL, options).then(handleHttpErrors);
+  return data;
+}
+
+export async function fetchPostJsonFormData(URL, form, token = null) {
+  let formElement = /** @type {HTMLFormElement} */ (form);
+  const formData = new FormData(formElement);
+  const dataFromForm = {};
+  formData.forEach((value, key) => (dataFromForm[key] = value));
+
+  let options = {
+    method: "POST",
+    body: JSON.stringify(dataFromForm),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (token) {
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+  const addedData = await fetch(URL, options).then(handleHttpErrors);
+  return addedData;
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, (error) => {
-    if (error) {
-        console.log("Error starting the server: " + error);
-        return;
-    }
-    console.log("Server is running on port: " + PORT);
+  if (error) {
+    console.log("Error starting the server: " + error);
+    return;
+  }
+  console.log("Server is running on port: " + PORT);
 });
